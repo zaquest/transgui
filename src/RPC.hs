4756879344597c6734fdc86504ae595df2c9ff11
zaquest <web.elektro.net@gmail.com>
2019-03-25 05:55:32 +0700

    Retry on invalid session id.

--- src/RPC.hs
+++ src/RPC.hs
@@ -6,10 +6,11 @@ import Control.Monad.Trans.Class (lift)
 import Control.Monad.Trans.Reader (ReaderT)
 import qualified Control.Monad.Trans.Reader as Reader
 import Control.Monad.IO.Class (MonadIO, liftIO)
-import Data.IORef (IORef, newIORef, readIORef)
+import Data.IORef (IORef, newIORef, readIORef, modifyIORef)
 import Network.HTTP.Client
 import Network.HTTP.Types
-import Data.ByteString (ByteString)
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LS
 
 
 data RPCSettings = RPCSettings
@@ -20,12 +21,48 @@ data RPCSettings = RPCSettings
 data RPCData = RPCData
   { dAddress :: String
   , dManager :: Manager
-  , dSessionId :: Maybe ByteString
+  , dSessionId :: Maybe BS.ByteString
   }
 
 
 newtype RPC a = RPC { unRPC :: ReaderT (IORef RPCData) IO a }
   deriving (Functor, Applicative, Monad, MonadIO)
+
+
+hSessionId :: HeaderName
+hSessionId = "X-Transmission-Session-Id"
+
+
+setSessionId :: Maybe BS.ByteString -> Request -> Request
+setSessionId Nothing req = req
+setSessionId (Just sessionId) req =
+  let hdrs = requestHeaders req
+   in req { requestHeaders = (hSessionId, sessionId) : hdrs }
+
+
+getHeader :: Response body -> HeaderName -> Maybe BS.ByteString
+getHeader resp hdr = snd <$> find ((hdr ==) . fst) (responseHeaders resp)
+
+
+saveSessionId :: Response body -> RPC ()
+saveSessionId resp = do
+  let hdr = getHeader resp hSessionId
+  rpcDataRef <- RPC Reader.ask
+  liftIO $ modifyIORef rpcDataRef (\d -> d { dSessionId = hdr })
+
+
+request :: RPC (Response LS.ByteString)
+request = do
+  d <- ask
+
+  req <- liftIO $ parseRequest (dAddress d)
+  let req' = setSessionId (dSessionId d) req
+  response <- liftIO $ httpLbs req' (dManager d)
+  saveSessionId response
+
+  case statusCode (responseStatus response) of
+    409 -> request
+    _ -> pure response
 
 
 runRPC :: RPCSettings -> RPC a -> IO a
@@ -46,16 +83,8 @@ ask :: RPC RPCData
 ask = asks id
 
 
-getHeader :: Response body -> HeaderName -> Maybe ByteString
-getHeader resp hdr = snd <$> find ((hdr ==) . fst) (responseHeaders resp)
-
-
-doshit :: RPC (Maybe ByteString)
+doshit :: RPC ()
 doshit = do
-  manager <- asks dManager
-  addr <- asks dAddress
-
-  request <- liftIO $ parseRequest addr
-  response <- liftIO $ httpLbs request manager
-
-  pure (getHeader response "X-Transmission-Session-Id")
+  resp <- request
+  liftIO $ putStrLn $ "The status code was: " ++ (show $ statusCode $ responseStatus resp)
+  liftIO $ print $ responseBody resp
